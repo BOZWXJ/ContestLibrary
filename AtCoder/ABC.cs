0c7829c9e2e21a33ad4a9f1d3951513c5b2995e6
BOZWXJ <s.mitamura@box.sakuraweb.com>
2020-06-25 16:22:57 +0900

    剰余演算子 負数対策 階乗 逆元 場合の数 追加

--- AtCoder/ABC.cs
+++ AtCoder/ABC.cs
@@ -15,77 +15,75 @@ namespace AtCoder
 			Console.Out.Flush();
 		}
 
-		private const int mod = 1000000007;  // 10^9+7
+		const int mod = 1000000007;  // 10^9+7
+		static int Mod(long x, int m) { return (int)((x % m + m) % m); }
+		static long Mod(long x, long m) { return (x % m + m) % m; }
 
 		static public void Solve(string[] args)
 		{
 			//string s = Console.ReadLine();
 			//int n = int.Parse(Console.ReadLine());
 			//long x = long.Parse(Console.ReadLine());
-			int[] vs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			//int[] vs = Console.ReadLine().Split().Select(int.Parse).ToArray();
 			//long[] vs = Console.ReadLine().Split().Select(long.Parse).ToArray();
 
-			long a = 12345678900000;
-			long b = 100000;
-			System.Diagnostics.Debug.WriteLine($"{a} / {b} = {a / b}");
-			a %= mod;
-			System.Diagnostics.Debug.WriteLine($"{a} / {b} = {a * ModInv(b, mod) % mod}");
+			int[] vs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			int n = vs[0];
+			int a = vs[1];
+			int b = vs[2];
 
-
-			//int n = vs[0];
-			//int a = vs[1];
-			//int b = vs[2];
-			//long ans = ModPow(2, n, mod) - 1;
+			long ans = ModPow(2, n, mod) - 1;
+			long x = Combination(n, a, mod);
+			long y = Combination(n, b, mod);
+			//System.Diagnostics.Debug.WriteLine($"{ans} {x} {y}");
+			ans = Mod(ans - x - y, mod);
 			//System.Diagnostics.Debug.WriteLine($"{ans}");
-			//ans -= Combination(n, a, mod);
-			//ans = (ans + mod) % mod;
-			//System.Diagnostics.Debug.WriteLine($"{ans}");
-			//ans -= Combination(n, b, mod);
-			//ans = (ans + mod) % mod;
-			//System.Diagnostics.Debug.WriteLine($"{ans}");
-			//Console.WriteLine(ans);
+			Console.WriteLine(ans);
 		}
 
-		#region 逆元（拡張Euclidの互除法） long ModInv(long a, long mod)
-		static long ModInv(long a, long mod)
+		#region 場合の数 O(r) long Combination(long n, long r, long mod)
+		static long Combination(int n, int r, long mod)
 		{
-			long result = 1;
-
-
-			return result;
+			if (n < r) { return 0; }
+			if (n == r) { return 1; }
+			// nCr = (n * (n-1) * ... * (n-(r-1))) / (r * r-1 * ... * 2 * 1)
+			long a = 1;
+			long b = 1;
+			for (int i = 0; i < r; i++) {
+				a = Mod(a * (n - i), mod);
+				b = Mod(b * (i + 1), mod);
+			}
+			return Mod(a * ModInv(b, mod), mod);
 		}
 		#endregion
 
-
+		#region 逆元(拡張Euclidの互除法) long ModInv(long a, long mod)
+		static long ModInv(long a, long m)
+		{
+			long b = m, x = 1, y = 0;
+			while (b > 0) {
+				long t = a / b;
+				a = Mod(a, b);
+				x = x - t * y;
+				(a, b, x, y) = (b, a, y, x);
+			}
+			x = Mod(x, m);
+			return x;
+		}
+		#endregion
 
 		#region 繰り返し二乗法 long ModPow(long x, long y, long mod)
 		static long ModPow(long x, long y, long mod)
 		{
 			long result = 1;
 			while (y > 0) {
-				if (y % 2 == 1) {
-					result = result * x % mod;
+				if (y % 2 != 0) {
+					result = Mod(result * x, mod);
 				}
-				x = x * x % mod;
+				x = Mod(x * x, mod);
 				y /= 2;
 			}
 			return result;
-		}
-		#endregion
-
-		#region 場合の数 long Combination(long n, long r, long mod)
-		static long Combination(long n, long r, long mod)
-		{
-			if (n < r) { return 0; }
-			r = Math.Min(r, n - r);
-			long ans = 1;
-			for (int i = (int)n; i > n - r; i--) {
-				ans *= i;
-			}
-			for (int i = 2; i <= r; i++) {
-				ans /= i;
-			}
-			return ans;
 		}
 		#endregion
 
--- AtCoder/MathRoutines.cs
+++ AtCoder/MathRoutines.cs
@@ -6,6 +6,8 @@ namespace ContestLibrary
 {
 	class MathRoutines
 	{
+		static int Mod(long x, int m) { return (int)((x % m + m) % m); }
+		static long Mod(long x, long m) { return (x % m + m) % m; }
 
 		#region 素数判定 bool IsPrime(long a)
 		public static bool IsPrime(long a)
@@ -47,35 +49,23 @@ namespace ContestLibrary
 		#region 最大公約数 long GCD(long a, long b)
 		public static long GCD(long a, long b)
 		{
-			if (b == 0) { return a; }
-
-			long tmp = a % b;
-			while (tmp != 0) {
-				a = b;
-				b = tmp;
-				tmp = a % b;
+			while (b != 0) {
+				a %= b;
+				(a, b) = (b, a);
 			}
-			return b;
+			return a;
 		}
 		#endregion
 
 		#region 最小公倍数 long LCM(long a, long b)
 		public static long LCM(long a, long b)
 		{
-			long gcd;
-			if (b == 0) {
-				gcd = a;
-			} else {
-				long x = a, y = b;
-				long tmp = x % y;
-				while (tmp != 0) {
-					x = y;
-					y = tmp;
-					tmp = x % y;
-				}
-				gcd = y;
+			long x = a, y = b;
+			while (y != 0) {
+				x %= y;
+				(x, y) = (y, x);
 			}
-			return a / gcd * b;
+			return a / x * b;
 		}
 		#endregion
 
@@ -84,34 +74,88 @@ namespace ContestLibrary
 		{
 			long result = 1;
 			while (y > 0) {
-				if (y % 2 == 1) {
-					result = result * x % mod;
+				if (y % 2 != 0) {
+					result = Mod(result * x, mod);
 				}
-				x = x * x % mod;
+				x = Mod(x * x, mod);
 				y /= 2;
 			}
 			return result;
 		}
 		#endregion
 
-		#region 逆元（Fermatの小定理） long ModInv1(long a, long mod)
-		static long ModInv1(long a, long mod)
+		#region 階乗(メモ付) long Factorial(int a, long mod)
+		static long Factorial(int a, long mod)
 		{
-			long b = mod - 2;
+			if (a < _Factorial.Count) {
+				return _Factorial[a];
+			}
+			int st = _Factorial.Count;
+			long result = st > 0 ? _Factorial[st - 1] : 1;
+			for (int i = st; i <= a; i++) {
+				result *= i;
+				result = Mod(result, mod);
+				_Factorial.Add(result);
+			}
+			return result;
+		}
+		static List<long> _Factorial = new List<long>(new long[] { 1 });
+		#endregion
+
+		// ax + py = 1
+		//	b = p
+		// ax + by = 1
+		//	a=qb+r
+		// (qb+r)x + by = 1
+		// qbx + rx + by = 1
+		// b(qx+y) + rx = 1
+		// b(qx+y) + rx = 1
+		//	a=b, b=r=a%b, x=x-qy=x-a/b*y ?, y=x
+		#region 逆元(拡張Euclidの互除法) long ModInv(long a, long mod)
+		static long ModInv(long a, long m)
+		{
+			long b = m, x = 1, y = 0;
+			while (b > 0) {
+				long t = a / b;
+				a = Mod(a, b);
+				x = x - t * y;
+				(a, b, x, y) = (b, a, y, x);
+			}
+			x = Mod(x, m);
+			return x;
+		}
+		#endregion
+
+		// a^p−1≡1(mod p)
+		#region 逆元(Fermatの小定理) long ModInv1(long a, long mod)
+		static long ModInv1(long a, long m)
+		{
+			long b = m - 2;
 			long result = 1;
 			while (b > 0) {
-				if (b % 2 == 1) {
-					result = result * a % mod;
+				if (b % 2 != 0) {
+					result = Mod(result * a, m);
 				}
-				a = a * a % mod;
+				a = Mod(a * a, m);
 				b /= 2;
 			}
 			return result;
 		}
 		#endregion
 
-
-
+		#region 拡張Euclidの互除法(再帰) ax + by = GCD(a,b)
+		static long ExtGCD(long a, long b, ref long x, ref long y)
+		{
+			if (b == 0) {
+				x = 1;
+				y = 0;
+				return a;
+			}
+			long d = ExtGCD(b, a % b, ref y, ref x);
+			y -= a / b * x;
+			return d;
+		}
+		#endregion
 
 		// 等差数列
 		// 和
@@ -120,14 +164,46 @@ namespace ContestLibrary
 		// nPr = n! / (n-r)!
 		// 組み合わせ
 		// nCr = nPr / r! = n! / r!(n-r)!
-		// nCr = (n * (n-1) * ...(n-(r-1))) / (1 * 2 * ...r)
+		// nCr = (n * (n-1) * ... * (n-(r-1))) / (r * r-1 * ... * 2 * 1)
+		//     = (n から n-(r-1) まで r個の総乗) / (r から 1 まで r個の総乗)
 		// 公式
 		// nCr = nCn-r
 		// nCr = n-1Cr + n-1Cr-1
 		// nC0 = 1
 		// nC1 = n
 
-		#region 場合の数（rが一桁程度） long Combination(int n, int r)
+		#region 場合の数 O(r) long Combination(long n, long r, long mod)
+		static long Combination(int n, int r, long mod)
+		{
+			if (n < r) { return 0; }
+			if (n == r) { return 1; }
+			// nCr = (n * (n-1) * ... * (n-(r-1))) / (r * r-1 * ... * 2 * 1)
+			long a = 1;
+			long b = 1;
+			for (int i = 0; i < r; i++) {
+				a = Mod(a * (n - i), mod);
+				b = Mod(b * (i + 1), mod);
+			}
+			return Mod(a * ModInv(b, mod), mod);
+		}
+		#endregion
+
+		#region 場合の数(階乗) long CombinationFactorial(long n, long r, long mod)
+		static long CombinationFactorial(int n, int r, long mod)
+		{
+			if (n < r) { return 0; }
+			if (n == r) { return 1; }
+			// n! / r!(n-r)!
+			long result = Factorial(n, mod);
+			result *= ModInv(Factorial(r, mod), mod);
+			result = Mod(result, mod);
+			result *= ModInv(Factorial(n - r, mod), mod);
+			result = Mod(result, mod);
+			return result;
+		}
+		#endregion
+
+		#region 場合の数(rが一桁程度) long Combination(int n, int r)
 		static long Combination(int n, int r)
 		{
 			if (n < r) { return 0; }
@@ -143,7 +219,7 @@ namespace ContestLibrary
 		}
 		#endregion
 
-		#region 場合の数（DP計算版） long CombinationDP(int n, int r, int mod)
+		#region 場合の数(DP計算版) long CombinationDP(int n, int r, int mod)
 		static long CombinationDP(int n, int r, int mod)
 		{
 			if (n < r) { return 0; }
@@ -160,7 +236,7 @@ namespace ContestLibrary
 		}
 		#endregion
 
-		#region 場合の数（Cache付きDP計算版） long CombinationCacheDP(int n, int r, int mod)
+		#region 場合の数(Cache付きDP計算版 long CombinationCacheDP(int n, int r, int mod)
 		static long CombinationCacheDP(int n, int r, int mod)
 		{
 			if (n < r) { return 0; }
